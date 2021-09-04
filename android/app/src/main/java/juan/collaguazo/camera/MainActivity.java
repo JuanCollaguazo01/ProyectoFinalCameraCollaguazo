3730919e841b9db1d7805e9b79d787d9247e14b6
Juan Collaguazo <juan.collaguazo01@epn.edu.ec>
2021-09-03 21:45:46 -0500

    Aplicacion

---
+++ android/app/src/main/java/juan/collaguazo/camera/MainActivity.java
@@ -0,0 +1,5 @@
+package juan.collaguazo.camera;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'juan.collaguazo.camera',
+  appName: 'JCameraJuanCollaguazo',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
